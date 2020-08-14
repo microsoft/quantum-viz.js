6f65157e0034d7dec9fed383af60c267b441a439
Raphael Koh <t-rkoh@microsoft.com>
2020-08-14 00:07:56 -0400

    Export Sqore as a class

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,7 @@ const config = {
         filename: '[name].js',
         library: 'Sqore',
         libraryTarget: 'var',
+        libraryExport: 'default',
     },
     // Add resolve for `tsx` and `ts` files, otherwise Webpack would
     // only look for common JavaScript file extension (.js)
