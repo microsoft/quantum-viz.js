16904573020e7ee14a2d48838dc9312d759bac0f
Jonathan Daniel <jonathan@classiq.io>
2021-11-23 11:11:46 +0200

    Install `qiskit` extras in nox tests session

--- quantum-viz/noxfile.py
+++ quantum-viz/noxfile.py
@@ -136,7 +136,7 @@ def mypy(session: Session) -> None:
 @session(python=python_versions)
 def tests(session: Session) -> None:
     """Run the test suite."""
-    session.install(".")
+    session.install(".[qiskit]")
     session.install("coverage[toml]", "pytest", "pygments")
     try:
         session.run("coverage", "run", "--parallel", "-m", "pytest", *session.posargs)
